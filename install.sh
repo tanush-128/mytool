c5c3d10dd1be5a13a2a5dced88a9d796ab34a557
tanush <tanush@myyahoo.com>
2024-08-29 13:03:44 +0530

    install.sh improved

--- install.sh
+++ install.sh
@@ -12,6 +12,18 @@ fi
 sudo cp -r bin "$INSTALL_DIR/"
 sudo cp tool.sh "$INSTALL_DIR/"
 
+# Set the correct permissions for the tool scripts
+sudo chmod +x "$INSTALL_DIR/tool.sh"
+sudo chmod +x "$INSTALL_DIR/bin/manage_server.sh"
+sudo chmod +x "$INSTALL_DIR/bin/setup_nginx.sh"
+sudo chmod +x "$INSTALL_DIR/bin/setup_ssl.sh"
+
+# Set ownership to root for security reasons
+sudo chown root:root "$INSTALL_DIR/tool.sh"
+sudo chown root:root "$INSTALL_DIR/bin/manage_server.sh"
+sudo chown root:root "$INSTALL_DIR/bin/setup_nginx.sh"
+sudo chown root:root "$INSTALL_DIR/bin/setup_ssl.sh"
+
 # Create a symbolic link to the main tool script in /usr/local/bin
 sudo ln -sf "$INSTALL_DIR/tool.sh" /usr/local/bin/mytool
 
